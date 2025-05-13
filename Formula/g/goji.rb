44ee785d6f20984bfc91f7545a06fd6d034d61c6
Matteo Juen <matteo.juen@ematric.com>
2025-05-13 10:45:21 +0200

    goji 1.4.1 (new formula)
    this commit adds a new formula for the CLI text emoticon maker goji. written in go
    
    goji 1.4.1 (new formula)
    
    this commit adds a new formula for the CLI text emoticon maker goji. written in go
    
    goji 1.4.1 (new formula)
    
    build from source instead of downloading
    
    goji 1.4.1 (new formula)
    
    this commit adds a new formula for the CLI text emoticon maker goji. written in go
    
    goji 1.4.1 (new formula)
    
    trim whitespace
    
    goji 1.4.1 (new formula)
    
    trim whitespace
    
    goji 1.4.1 (new formula)
    
    line spacing changes
    
    Co-authored-by: Anton Melnikov <antonsm21@gmail.com>
    
    goji 1.4.3 (new forumula)
    
    goji v1.4.3 (new formula)
    
    bump version
    
    goji v1.4.3 (new formula)
    
    remove comments

---
+++ Formula/g/goji.rb
@@ -0,0 +1,20 @@
+class Goji < Formula
+  desc "Lightweight version of Oji text emoticon generator. Rewritten in go"
+  homepage "https://github.com/sanriodev/goji"
+  url "https://github.com/sanriodev/goji/archive/refs/tags/v1.4.3.tar.gz"
+  sha256 "001f77b5fe67cc9c1243977f020b7f6162be0d5cae46b18564048f1f5bafe331"
+  license "MIT"
+
+  depends_on "go" => :build
+
+  def install
+    system "go", "build", *std_go_args
+  end
+
+  test do
+    output = shell_output("#{bin}/goji -h")
+    assert_match(/Create custom or random text emojis/, output)
+    assert_match(/--new/, output)
+    assert_match(/--random/, output)
+  end
+end
